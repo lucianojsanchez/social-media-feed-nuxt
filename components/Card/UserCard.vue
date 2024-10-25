efca482027c990b64275bf853d694087d32cf38b
lucianojsanchez <cbrook3n@gmail.com>
2024-10-25 09:19:18 -0300

    deleted pending from fetch, due the app fast response

--- components/Card/UserCard.vue
+++ components/Card/UserCard.vue
@@ -4,10 +4,11 @@
       class="grid grid-cols-1 gap-6 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2"
     >
       <div
-        class="bg-[#1E1E1E] border-slate-800 shadow-2xl rounded-lg max-w-md p-4"
-        v-for="(user, index) in users.slice(0, 40)"
+        class="bg-[#1E1E1E] border-slate-800 shadow-2xl hover:shadow-gray-500 rounded-lg max-w-md p-4"
+        v-for="(user, index) in users.slice(0, 100)"
         :key="index"
       >
+        <div v-if="users.length === 0">{{ error?.message }}</div>
         <div class="flex items-center px-4 py-3">
           <img
             :src="user.picture.large"
@@ -21,14 +22,33 @@
               {{ user.name.first }} {{ user.name.last }}
             </span>
             <span class="text-gray-400 text-xs block"
-              >{{ user.location.city }}, {{ user.location.state }}</span
-            >
+              >{{ user.location.city }}, {{ user.location.state }}
+            </span>
           </div>
         </div>
-        <img :src="posts[index]?.image" alt="Post image" />
+        <div
+          class="relative w-full h-[365px] w-[416px] flex items-center justify-center"
+        >
+          <img
+            v-if="posts[index]?.image"
+            :src="posts[index]?.image"
+            :alt="posts[index]?.owner"
+            class="w-full h-full object-cover rounded-lg"
+          />
+          <div
+            v-else
+            class="flex items-center justify-center w-full h-full bg-gray-800"
+          >
+            Loading...
+          </div>
+        </div>
         <div class="flex items-center justify-between mx-4 mt-3 mb-2">
           <div class="flex gap-5">
-            <img :src="thumbsUp" class="" />
+            <button
+              class="hover:bg-red-600 active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300"
+            >
+              <img :src="thumbsUp" />
+            </button>
           </div>
         </div>
       </div>
@@ -41,6 +61,6 @@ import { useFetchUsers } from "@/composables/useFetchUsers";
 import { useGeneratePost } from "@/composables/useFetchUsers";
 import thumbsUp from "@/assets/thumbsUp.svg";
 
-const { users, error, pending } = useFetchUsers();
+const { users, error } = useFetchUsers();
 const posts = useGeneratePost(users);
 </script>
--- composables/useFetchUsers.ts
+++ composables/useFetchUsers.ts
@@ -12,26 +12,23 @@ export interface User {
     thumbnail: string;
   };
 }
-
 export interface Post {
   image: string;
   owner: string;
 }
 
 export function useFetchUsers() {
-  const { data, error, pending } = useFetch<{ results: User[] }>(
-    "https://randomuser.me/api/?results=40"
+  const { data, error } = useFetch<{ results: User[] }>(
+    "https://randomuser.me/api/?results=100"
   );
-
   return {
     users: data.value ? data.value.results : [],
     error,
-    pending,
   };
 }
 
 function getRandomImageUrl(): string {
-  const randomId = Math.floor(Math.random() * 100);
+  const randomId = Math.floor(Math.random() * 50);
   return `https://picsum.photos/id/${randomId}/1024/736`;
 }
 
